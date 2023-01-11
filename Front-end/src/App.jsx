626513b54fbbecda13b49683b5b7b3bce95dc8db
NoraSEDJAI <nora.sedj@gmail.com>
2023-01-11 12:40:46 +0100

    Create Front Footer - Nora Sedjai
    Score : 2
    Create front of footer in React JS.

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,9 +1,14 @@
 import "../src/App.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/footer";
+
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+    <footer>
+      <Footer />
+    </footer>
     </div>
   );
 };
