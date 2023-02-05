3da725821e536ca1a5400bfcfecc69d8a1cd9593
sckaper <verglacire@gmail.com>
2023-02-05 22:36:14 +0100

    feature : add component carousel
    Add Component carousel and the style of the images

--- Front-end/src/components/Carousel.jsx
+++ Front-end/src/components/Carousel.jsx
@@ -1,15 +1,29 @@
 import React from "react"
 import "../style/Carousel.css"
 import "react-responsive-carousel/lib/styles/carousel.min.css"
-import { Carousel } from 'react-responsive-carousel'
+import { Carousel } from "react-responsive-carousel"
 
 const CarouselComponent = (props) => {
   const { images } = props
-  return  (
+  return (
     <div class="carousel">
-      <Carousel useKeyboardArrows={true}>
-      {images.map((image, index) => (
-        <img alt="carouselImage" src={image} key={index}/>
+      <Carousel
+        useKeyboardArrows={true}
+        showThumbs={false}
+        showIndicators={false}
+        autoPlay={true}
+        showStatus={false}
+        autoplay
+        transitionTime="500"
+        interval="2000"
+      >
+        {images.map((image, index) => (
+          <img
+            class="carouselImage"
+            alt="carouselImage"
+            src={image}
+            key={index}
+          />
         ))}
       </Carousel>
     </div>
