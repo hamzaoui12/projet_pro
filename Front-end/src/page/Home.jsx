6eec1bf7602951036bd5df1129c150304de76dcd
Mina <94831246+amdz213@users.noreply.github.com>
2023-03-09 10:45:56 +0100

    page Home

--- Front-end/src/page/Home.jsx
+++ Front-end/src/page/Home.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Carousel from "../components/Carousel";
+import Footer from "../components/Footer";
+
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Carousel/>
+      <Footer/>
     </div>
   );
 };
