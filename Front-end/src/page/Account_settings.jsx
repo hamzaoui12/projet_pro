27a3bfd0ff909753cead4a39d1cf0358f0962f71
NoraSEDJAI <nora.sedj@gmail.com>
2023-04-09 22:42:00 +0200

    premier front de la page account settings mais je rencontre des problemes avec tailwind css. Donc, il reste des choses à faire

---
+++ Front-end/src/page/Account_settings.jsx
@@ -0,0 +1,139 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+function Account_settings() {
+  return (
+    <div className="flex justify-center">
+      <div className="w-full max-w-md">
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          validationSchema={Yup.object({
+            email: Yup.string()
+              .email('Invalid email address')
+              .required('Required'),
+            password: Yup.string()
+              .min(6, 'Must be at least 6 characters')
+              .required('Required'),
+          })}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+              <div className="mb-4">
+                <label
+                  htmlFor="prenom"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Prenom
+                </label>
+                <Field
+                  type="prenom"
+                  name="prenom"
+                  id="prenom"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+                <ErrorMessage
+                  name="prenom"
+                  component="prenom"
+                  className="text-red-500 text-xs italic"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="nom"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Nom
+                </label>
+                <Field
+                  type="nom"
+                  name="nom"
+                  id="nom"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+                <ErrorMessage
+                  name="nom"
+                  component="nom"
+                  className="text-red-500 text-xs italic"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="phone"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Phone number
+                </label>
+                <Field
+                  type="phone"
+                  name="phone"
+                  id="phone"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+                <ErrorMessage
+                  name="phone"
+                  component="phone"
+                  className="text-red-500 text-xs italic"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="email"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Email
+                </label>
+                <Field
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="text-red-500 text-xs italic"
+                />
+              </div>
+              <div className="mb-6">
+                <label
+                  htmlFor="password"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Password
+                </label>
+                <Field
+                  type="password"
+                  name="password"
+                  id="password"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="text-red-500 text-xs italic"
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                >
+                  Submit
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
+}
+
+export default Account_settings;
