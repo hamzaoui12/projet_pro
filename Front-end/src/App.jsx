f15356b88a50747a8f4874a62d83131599956cd5
merina hamzaoui <hamzaouimerina@gmail.com>
2023-05-14 21:19:34 +0200

    fontion ajouter au panier

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,12 +1,14 @@
 import "../src/App.css"
 import Navbar from "./components/Navbar"
 import Category from "./page/Category"
+import CarouselComponent from "./components/Carousel"
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
       <Category />
+      <CarouselComponent images={[]} duration={1000} className="h-12 l-12" />
     </div>
   )
 }
