d380da4ff22cff7db55f2fe9f8ee0ca890ad52e4
merina hamzaoui <hamzaouimerina@gmail.com>
2023-05-14 22:40:03 +0200

    creation de la fonction déja un compte

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,12 +1,13 @@
 import "../src/App.css"
 import Navbar from "./components/Navbar"
-import Category from "./page/Category"
+import Inscription from "./page/Inscription"
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
-      <Category />
+
+      <Inscription />
     </div>
   )
 }
