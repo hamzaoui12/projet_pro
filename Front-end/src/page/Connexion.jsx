ed67360c0348849199b7f1d60e1b03038fbb375c
Mina <94831246+amdz213@users.noreply.github.com>
2023-05-19 19:12:19 +0200

    modification du code de la page , et ajout du style

--- Front-end/src/page/Connexion.jsx
+++ Front-end/src/page/Connexion.jsx
@@ -1,40 +1,66 @@
-import React from "react";
+import React, { useState } from 'react';
 
 const Connexion = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const dispatch = useDispatch();
-    const { isFetching, error } = useSelector((state) => state.user);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
   
-    const handleClick = (e) => {
-      e.preventDefault();
-      Connexion(dispatch, { username, password });
-    };
-    return (
-      <container>
-        <wrapper>
-          <title> Connexion </title>
-          <form>
-            <input
-              placeholder="username"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <input
-              placeholder="password"
-              type="password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <button onClick={handleClick} disabled={isFetching}>
-              CONNEXION
-            </button>
-            {error && <Error>Echec de la Connexion...</Error>}
-            <link>Mot de passe oublier !</link>
-            <link>Creer un nouveau compte</link>
-          </form>
-        </wrapper>
-      </container>
-    );
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Effectuez ici la vérification de l'email et du mot de passe dans votre système d'authentification
+    // Si l'authentification réussit, redirigez l'utilisateur vers la page privée qu'il souhaitait initialement
+    // Sinon, affichez une erreur
+   
   };
-  
-  export default Connexion;
-  
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleLogin}>
+        <h2 className="text-2xl mb-4">Connexion</h2>
+        {error && <p className="text-red-500 mb-4">{error}</p>}
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            Mot de passe
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Mot de passe"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <p className="text-blue-500 text-sm">
+            
+          </p>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Se connecter
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Connexion;
