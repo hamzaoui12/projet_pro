d8e8d8ee18863eb8ac174f44b1a8dd6d39fffed0
Mina <94831246+amdz213@users.noreply.github.com>
2023-05-20 00:47:16 +0200

    ajout du style a la page inscription et modification du code

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -2,6 +2,7 @@ import "../src/App.css"
 import Navbar from "./components/Navbar"
 import Category from "./page/Category"
 import CarouselComponent from "./components/Carousel"
+import Inscription from "./page/Inscription"
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Category />
       <CarouselComponent images={[]} duration={1000} className="h-12 l-12" />
+      <Inscription/>
     </div>
   )
 }
--- Front-end/src/page/Inscription.jsx
+++ Front-end/src/page/Inscription.jsx
@@ -1,25 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-  const Inscription = () => {
-    return (
-      <container>
-        <wrapper>
-          <title>Creer un compte</title>
-          <form>
-            <input placeholder="Nom" />
-            <input placeholder="Prenom" />
-            <input placeholder="Nom d'utilisateur" />
-            <input placeholder="Email" />
-            <input placeholder="Mot de passe" />
-            <input placeholder="Confirmation du mot de passe" />
-            <agreement>
-            En créant un compte, je consens au traitement de mes données personnelles
-            données conformément à la politique de confidentialité
-            </agreement>
-            <button>Creer</button>
-          </form>
-        </wrapper>
-      </container>
-    );
+const Inscription = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
   };
-  export default Inscription;
+
+  return (
+    <div className="w-full h-screen flex items-center" style={{ backgroundColor: "#F5F2E8" }}>
+      <div className="w-1/2 mx-auto bg-white p-8 rounded-lg shadow-lg relative">
+        <h1 className="text-4xl font-semibold mb-8 text-center">Créer un compte</h1>
+        <form className="flex flex-col space-y-4">
+          <input className="px-4 py-2 border border-gray-300 rounded-lg outline-none" placeholder="Nom" />
+          <input className="px-4 py-2 border border-gray-300 rounded-lg outline-none" placeholder="Prénom" />
+          <input className="px-4 py-2 border border-gray-300 rounded-lg outline-none" placeholder="Nom d'utilisateur" />
+          <input className="px-4 py-2 border border-gray-300 rounded-lg outline-none" placeholder="Email" />
+          <input className="px-4 py-2 border border-gray-300 rounded-lg outline-none" placeholder="Mot de passe" />
+          <input className="px-4 py-2 border border-gray-300 rounded-lg outline-none" placeholder="Confirmation du mot de passe" />
+          <div className="flex items-center">
+            <input
+              type="checkbox"
+              className="mr-2"
+              checked={isChecked}
+              onChange={handleCheckboxChange}
+            />
+            <p className="text-sm text-gray-600">
+              En créant un compte, je consens au traitement de mes données personnelles données conformément à la politique de confidentialité.
+            </p>
+          </div>
+          <button className="w-full bg-black text-white rounded-md py-3 font-semibold hover:bg-gray-800">
+            Créer
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Inscription;
