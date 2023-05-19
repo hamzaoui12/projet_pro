543a8d5c50e4d9b4c6a308fd565d78137444fba2
Mina <94831246+amdz213@users.noreply.github.com>
2023-05-19 23:28:43 +0200

    ajout du style et restructuration de mon code

--- Front-end/src/page/Connexion.jsx
+++ Front-end/src/page/Connexion.jsx
@@ -1,66 +1,61 @@
-import React, { useState } from 'react';
+import React from "react";
 
 const Connexion = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-    // Effectuez ici la vérification de l'email et du mot de passe dans votre système d'authentification
-    // Si l'authentification réussit, redirigez l'utilisateur vers la page privée qu'il souhaitait initialement
-    // Sinon, affichez une erreur
-   
-  };
-
   return (
-    <div className="flex items-center justify-center h-screen">
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleLogin}>
-        <h2 className="text-2xl mb-4">Connexion</h2>
-        {error && <p className="text-red-500 mb-4">{error}</p>}
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-            Email
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="email"
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+    <div className="w-full h-screen flex items-center">
+      <div className="relative w-1/2 h-full flex flex-col">
+        <div className="absolute top-[20%] left-[10%] flex flex-col">
+          <h1 className="text-6xl text-white font-bold my-4 tracking-wide leading-tight">
+            MAKE YOUR <span className="text-[#FED7AA]">FURNITURE</span> DREAMS A <span className="text-[#FED7AA]">REALITY</span>
+          </h1>
+          <p className="text-2xl text-black font-normal">Logging in to get more offers on our articles !</p>
         </div>
-        <div className="mb-6">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-            Mot de passe
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            type="password"
-            placeholder="Mot de passe"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <p className="text-blue-500 text-sm">
-            
-          </p>
+        <img src="https://images.pexels.com/photos/2724748/pexels-photo-2724748.jpeg" className="w-full h-full object-cover" />
+      </div>
+      <div className="w-1/2 h-full bg-[#F5f5F5] flex flex-col p-20 justify-center">
+        <div className="w-full flex flex-col">
+          <div className="w-full flex flex-col mb-5">
+            <h3 className="text-6xl font-bold mb-4 ">Login</h3>
+            <p className="text-base mb-2  ">Welcome Back! Please enter your details.</p>
+          </div>
+          <div className="w-full flex flex-col">
+            <input
+              type="email"
+              placeholder="Email"
+              className="w-full text-black py-4 bg-none border-black outline-none focus:outline-none"
+            />
+          </div>
+          <div className="w-full flex flex-col my-4">
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-full text-black py-4 bg-none border-black outline-none focus:outline-none"
+            />
+          </div>
+          <div className="w-full flex items-end justify-end mb-4">
+            <p className="text-base font-medium whitespace-normal cursor-pointer underline underline-offset-2">Forgot Password?</p>
+          </div>
+          <div className="w-full flex flex-col my-6">
+            <div className="flex justify-center">
+              <button className="w-full text-white m-1 bg-black rounded-md p-4 text-center flex items-center justify-center text-xl font-semibold">
+                Log in
+              </button>
+            </div>
+            <div className="flex justify-center">
+              <button className="w-full text-[#060606] m-1 bg-white border-2 border-black rounded-md p-4 text-center flex items-center text-xl font-semibold">
+                Register
+              </button>
+            </div>
+          </div>
+          <div className="w-full flex items-center justify-center">
+            <p className="text-base font-normal text-[#060606]">
+              Don't have an account? <span className="font-semibold underline underline-offset">Register</span>
+            </p>
+          </div>
         </div>
-        <div className="flex items-center justify-between">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
-          >
-            Se connecter
-          </button>
-        </div>
-      </form>
+      </div>
     </div>
   );
-};
+}
 
 export default Connexion;
