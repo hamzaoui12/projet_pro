214981be4ab3dbf6d4d00a743277e8032b13882f
merina hamzaoui <hamzaouimerina@gmail.com>
2023-05-22 23:26:29 +0200

    mise a jour du style de la page rechrche

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,20 +1,11 @@
 import "../src/App.css"
 import Navbar from "./components/Navbar"
-import Category from "./page/Category"
-import Product from "./page/Product"
-import CarouselComponent from "./components/Carousel"
-import CategoryComposant from "./components/CategoryComposant"
-import Connexion from "./page/Connexion"
-
+import Research from "./page/Research"
 const App = () => {
   return (
     <div className="app">
       <Navbar />
-      <Category />
-      <Product />
-      <CarouselComponent images={[]} duration={1000} className="h-12 l-12" />
-      <CategoryComposant />
-      <Connexion />
+      <Research />
     </div>
   )
 }
