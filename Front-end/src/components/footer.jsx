fe697170f1c813aaf0d65a3bae6ccdb950b893bb
NoraSEDJAI <nora.sedj@gmail.com>
2023-05-24 21:50:36 +0200

    add icone footer

--- Front-end/src/components/footer.jsx
+++ Front-end/src/components/footer.jsx
@@ -1,43 +1,44 @@
 import React from 'react';
+import { FaInstagram, FaFacebook, FaTwitter } from 'react-icons/fa';
 
 const Footer = () => (
   <footer className="bg-black text-white">
     <div className="container mx-auto py-4">
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
         <div>
-          <h6 className="text-lg font-bold underline">CGU</h6>
+          <h6 className="text-lg font-bold underline mb-2">CGU</h6>
           <ul className="text-sm">
             <li> © Inter ARNEIS Systems B.V 2022-2023</li>
           </ul>          
         </div>
         <div>
-          <h3 className="text-lg font-bold underline">Mentions légales</h3>
+          <h3 className="text-lg font-bold underline mb-2">Mentions légales</h3>
           <ul className="text-sm">
             <li>Dénomination sociale : Meubles ARNEIS France S.A.S.</li>
             <li>Siège social : 425 rue Henri Barbusse 78370 PLAISIR</li>
           </ul>
         </div>
         <div>
-          <h3 className="text-lg font-bold underline">Contact</h3>
+          <h3 className="text-lg font-bold underline mb-2">Contact</h3>
           <p>Numéro de téléphone : <a href="tel:+123456789">+123456789</a></p>
           <p>Email : <a href="mailto:contact@example.com">contact@example.com</a></p>
         </div>
         <div>
-          <h3 className="text-lg font-bold underline">Réseaux sociaux</h3>
+          <h3 className="text-lg font-bold underline mb-2">Réseaux sociaux</h3>
           <ul className="flex space-x-4">
             <li>
               <a href="https://www.instagram.com/">
-                <img src="chemin_vers_icone_instagram.png" alt="Instagram" />
+                <FaInstagram size={24} color="white" />
               </a>
             </li>
             <li>
               <a href="https://www.facebook.com/">
-                <img src="chemin_vers_icone_facebook.png" alt="Facebook" />
+                <FaFacebook size={24} color="white" />
               </a>
             </li>
             <li>
               <a href="https://twitter.com/">
-                <img src="chemin_vers_icone_twitter.png" alt="Twitter" />
+                <FaTwitter size={24} color="white" />
               </a>
             </li>
           </ul>
