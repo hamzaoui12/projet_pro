30a376afdae9b0efd0f20a22e5de3ae9e7da6a47
NoraSEDJAI <nora.sedj@gmail.com>
2023-05-25 17:12:30 +0200

    form update account

--- Front-end/src/page/Account_settings.jsx
+++ Front-end/src/page/Account_settings.jsx
@@ -1,139 +1,98 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
+import React, { useState } from "react";
+import "tailwindcss/tailwind.css";
 
-function Account_settings() {
+const Registration = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const userExists = checkIfUserExists(username);
+    const emailExists = checkIfEmailExists(email);
+
+    if (userExists || emailExists) {
+      setErrorMessage(
+        "An account with this username or email already exists."
+      );
+    } else {
+      // Handle form submission
+    }
+  };
+
+  const checkIfUserExists = (username) => {
+    return false;
+  };
+
+  const checkIfEmailExists = (email) => {
+    return false;
+  };
+
   return (
-    <div className="flex justify-center">
-      <div className="w-full max-w-md">
-        <Formik
-          initialValues={{ email: '', password: '' }}
-          validationSchema={Yup.object({
-            email: Yup.string()
-              .email('Invalid email address')
-              .required('Required'),
-            password: Yup.string()
-              .min(6, 'Must be at least 6 characters')
-              .required('Required'),
-          })}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              setSubmitting(false);
-            }, 400);
-          }}
-        >
-          {({ isSubmitting }) => (
-            <Form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-              <div className="mb-4">
-                <label
-                  htmlFor="prenom"
-                  className="block text-gray-700 font-bold mb-2"
-                >
-                  Prenom
-                </label>
-                <Field
-                  type="prenom"
-                  name="prenom"
-                  id="prenom"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-                <ErrorMessage
-                  name="prenom"
-                  component="prenom"
-                  className="text-red-500 text-xs italic"
-                />
-              </div>
-              <div className="mb-4">
-                <label
-                  htmlFor="nom"
-                  className="block text-gray-700 font-bold mb-2"
-                >
-                  Nom
-                </label>
-                <Field
-                  type="nom"
-                  name="nom"
-                  id="nom"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-                <ErrorMessage
-                  name="nom"
-                  component="nom"
-                  className="text-red-500 text-xs italic"
-                />
-              </div>
-              <div className="mb-4">
-                <label
-                  htmlFor="phone"
-                  className="block text-gray-700 font-bold mb-2"
-                >
-                  Phone number
-                </label>
-                <Field
-                  type="phone"
-                  name="phone"
-                  id="phone"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-                <ErrorMessage
-                  name="phone"
-                  component="phone"
-                  className="text-red-500 text-xs italic"
-                />
-              </div>
-              <div className="mb-4">
-                <label
-                  htmlFor="email"
-                  className="block text-gray-700 font-bold mb-2"
-                >
-                  Email
-                </label>
-                <Field
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-                <ErrorMessage
-                  name="email"
-                  component="div"
-                  className="text-red-500 text-xs italic"
-                />
-              </div>
-              <div className="mb-6">
-                <label
-                  htmlFor="password"
-                  className="block text-gray-700 font-bold mb-2"
-                >
-                  Password
-                </label>
-                <Field
-                  type="password"
-                  name="password"
-                  id="password"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-                <ErrorMessage
-                  name="password"
-                  component="div"
-                  className="text-red-500 text-xs italic"
-                />
-              </div>
-              <div className="flex items-center justify-between">
-                <button
-                  type="submit"
-                  disabled={isSubmitting}
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                >
-                  Submit
-                </button>
-              </div>
-            </Form>
-          )}
-        </Formik>
+    <div
+      className="flex items-center justify-end h-screen bg-cover bg-center"
+      style={{
+        backgroundImage:
+          'url("https://images.pexels.com/photos/2029665/pexels-photo-2029665.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
+      }}
+    >
+      <div className="bg-white bg-opacity-80 p-6 rounded-lg shadow-md mr-10">
+        <h1 className="text-4xl font-bold mb-8 text-gray-800">Personal Information</h1>
+        <form className="flex flex-col gap-4">
+          <input
+            className="bg-transparent border border-gray-500 p-2 rounded-lg"
+            placeholder="First Name"
+          />
+          <input
+            className="bg-transparent border border-gray-500 p-2 rounded-lg"
+            placeholder="Last Name"
+          />
+          <input
+            className="bg-transparent border border-gray-500 p-2 rounded-lg"
+            placeholder="Username"
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <input
+            className="bg-transparent border border-gray-500 p-2 rounded-lg"
+            placeholder="Email"
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <input
+            className="bg-transparent border border-gray-500 p-2 rounded-lg"
+            placeholder="Password"
+          />
+          <input
+            className="bg-transparent border border-gray-500 p-2 rounded-lg"
+            placeholder="Confirm Password"
+          />
+        </form>
+
+        <h1 className="text-4xl font-bold mb-8 text-gray-800">Address Book</h1>
+        <form className="flex flex-col gap-4">
+          <input
+            className="bg-transparent border border-gray-500 p-2 rounded-lg"
+            placeholder="Shipping Address"
+          />
+          <input
+            className="bg-transparent border border-gray-500 p-2 rounded-lg"
+            placeholder="Billing Address"
+          />
+        </form>
+        <h1 className="text-4xl font-bold mb-8 text-gray-800">Payment Methods</h1>
+        <form className="flex flex-col gap-4 pb-4">
+          <input
+            className="bg-transparent border border-gray-500 p-2 rounded-lg"
+            placeholder="Payment Methods"
+          />
+        </form>
+
+        <button className="bg-black text-white py-2 px-4 rounded-lg hover:bg-gray-700 transition-all duration-200">
+          Update
+        </button>
       </div>
     </div>
   );
-}
+};
 
-export default Account_settings;
+export default Registration;
