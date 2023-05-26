e908c68e4378bde42aa69b81714c02290b7ff17b
Mina <94831246+amdz213@users.noreply.github.com>
2023-05-26 16:30:30 +0200

    modif

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,12 +1,13 @@
 import "../src/App.css"
 import Research from "./page/Research"
-
+import Connexion from "./page/Connexion"
 import Footer from "./components/footer"
 
 const App = () => {
   return (
     <div className="app">
       <Research />
+      <Connexion/>
       <footer>
         <Footer />
       </footer>
