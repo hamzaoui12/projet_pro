a2a455f7ff0a2f6c6d6a31bffa1768aeb735a221
NoraSEDJAI <nora.sedj@gmail.com>
2023-05-29 17:42:48 +0200

    Update front center form

--- Front-end/src/page/Account_settings.jsx
+++ Front-end/src/page/Account_settings.jsx
@@ -31,39 +31,39 @@ const Account = () => {
 
   return (
     <div
-      className="flex items-center justify-end h-screen bg-cover bg-center"
+      className="flex items-center justify-center h-screen bg-cover bg-center"
       style={{
         backgroundImage:
           'url("https://images.pexels.com/photos/2029665/pexels-photo-2029665.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
       }}
     >
-      <div className="bg-white bg-opacity-80 p-6 rounded-lg shadow-md mr-10">
+      <div className="bg-white bg-opacity-80 p-6 rounded-lg shadow-md max-w-lg mx-auto">
         <h1 className="text-4xl font-bold mb-8 text-gray-800">Personal Information</h1>
         <form className="flex flex-col gap-4">
           <input
-            className="bg-transparent border border-gray-500 p-2 rounded-lg"
+            className="bg-transparent border border-gray-500 p-2 rounded-lg w-full"
             placeholder="First Name"
           />
           <input
-            className="bg-transparent border border-gray-500 p-2 rounded-lg"
+            className="bg-transparent border border-gray-500 p-2 rounded-lg w-full"
             placeholder="Last Name"
           />
           <input
-            className="bg-transparent border border-gray-500 p-2 rounded-lg"
+            className="bg-transparent border border-gray-500 p-2 rounded-lg w-full"
             placeholder="Username"
             onChange={(event) => setUsername(event.target.value)}
           />
           <input
-            className="bg-transparent border border-gray-500 p-2 rounded-lg"
+            className="bg-transparent border border-gray-500 p-2 rounded-lg w-full"
             placeholder="Email"
             onChange={(event) => setEmail(event.target.value)}
           />
           <input
-            className="bg-transparent border border-gray-500 p-2 rounded-lg"
+            className="bg-transparent border border-gray-500 p-2 rounded-lg w-full"
             placeholder="Password"
           />
           <input
-            className="bg-transparent border border-gray-500 p-2 rounded-lg"
+            className="bg-transparent border border-gray-500 p-2 rounded-lg w-full"
             placeholder="Confirm Password"
           />
         </form>
@@ -71,18 +71,18 @@ const Account = () => {
         <h1 className="text-4xl font-bold mb-8 text-gray-800">Address Book</h1>
         <form className="flex flex-col gap-4">
           <input
-            className="bg-transparent border border-gray-500 p-2 rounded-lg"
+            className="bg-transparent border border-gray-500 p-2 rounded-lg w-full"
             placeholder="Shipping Address"
           />
           <input
-            className="bg-transparent border border-gray-500 p-2 rounded-lg"
+            className="bg-transparent border border-gray-500 p-2 rounded-lg w-full"
             placeholder="Billing Address"
           />
         </form>
         <h1 className="text-4xl font-bold mb-8 text-gray-800">Payment Methods</h1>
         <form className="flex flex-col gap-4 pb-4">
           <input
-            className="bg-transparent border border-gray-500 p-2 rounded-lg"
+            className="bg-transparent border border-gray-500 p-2 rounded-lg w-full"
             placeholder="Payment Methods"
           />
         </form>
