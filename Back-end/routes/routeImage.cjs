844cd44397b3db94802509b40bef002cde4a2ba1
NoraSEDJAI <nora.sedj@gmail.com>
2023-06-10 19:02:27 +0200

    Pass linter

--- Back-end/routes/routeImage.cjs
+++ Back-end/routes/routeImage.cjs
@@ -1,66 +1,68 @@
-const ImageModel = require("../models/ImageModel.cjs");
-const auth = require("../middlewares/auth.js");
-const multer = require("multer");
+const ImageModel = require("../models/ImageModel.cjs")
+const multer = require("multer")
 
 const routeImages = async ({ app, db }) => {
   const checkImage = (image) => {
     if (image) {
-      return true;
+      return true
     }
-    return false;
-  };
+    
+return false
+  }
 
   app.get("/images", async (req, res) => {
     res.send({
       result: await ImageModel.query(),
-    });
-  });
+    })
+  })
 
   app.get("/images/:id", async (req, res) => {
-    const { id } = req.params;
-    const image = await ImageModel.query().findById(id);
+    const { id } = req.params
+    const image = await ImageModel.query().findById(id)
 
     if (!checkImage(image)) {
-      res.status(404).send({ error: "not found" });
-      return;
+      res.status(404).send({ error: "not found" })
+
+      
+return
     }
 
-    res.send({ result: image });
-  });
+    res.send({ result: image })
+  })
 
     // Configurez Multer pour spécifier où enregistrer les fichiers téléchargés
   const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "uploads/");
+      cb(null, "uploads/")
     },
     filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-      cb(null, file.fieldname + "-" + uniqueSuffix + ".jpg");
+      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9)
+      cb(null, file.fieldname + "-" + uniqueSuffix + ".jpg")
     },
-  });
+  })
 
-  const upload = multer({ storage: storage });
+  const upload = multer({ storage: storage })
 
   app.post("/images", upload.single("picture"), async (req, res) => {
-    const { product_id, category_id } = req.body;
-    const picture = req.file.filename;
+    const { product_id, category_id } = req.body
+    const picture = req.file.filename
 
     try {
       const newImage = await ImageModel.query().insert({
         product_id,
         category_id,
         picture,
-      });
+      })
 
-      res.status(201).send({ result: newImage });
+      res.status(201).send({ result: newImage })
     } catch (error) {
-      res.status(500).send({ error: "Failed to add image" });
+      res.status(500).send({ error: "Failed to add image" })
     }
-  });
+  })
 
   app.patch("/images/:id", async (req, res) => {
-    const { id } = req.params;
-    const { product_id, category_id, picture } = req.body;
+    const { id } = req.params
+    const { product_id, category_id, picture } = req.body
 
     try {
       const updateImage = await ImageModel.query()
@@ -68,32 +70,38 @@ const routeImages = async ({ app, db }) => {
           product_id,
           category_id,
           picture,
-        });
+        })
 
       if (!checkImage(updateImage)) {
-        res.status(404).send({ error: "not found" });
-        return;
+        res.status(404).send({ error: "not found" })
+
+        
+return
       }
 
-      res.send(updateImage);
+      res.send(updateImage)
     } catch (error) {
-      res.send({ result: error });
-      return;
+      res.send({ result: error })
+
+      
+return
     }
-  });
+  })
 
   app.delete("/images/:id", async (req, res) => {
-    const { id } = req.params;
-    const [image] = await db("images").where({ id });
+    const { id } = req.params
+    const [image] = await db("images").where({ id })
 
     if (!checkImage(image)) {
-      res.status(404).send({ error: "not found" });
-      return;
+      res.status(404).send({ error: "not found" })
+
+      
+return
     }
 
-    res.send({ result: image });
-    await ImageModel.query().deleteById(id);
-  });
-};
+    res.send({ result: image })
+    await ImageModel.query().deleteById(id)
+  })
+}
 
-module.exports = routeImages;
+module.exports = routeImages
