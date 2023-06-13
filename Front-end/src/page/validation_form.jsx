14a0ef5340a459755e453971fad0b66d67061c47
Mina <94831246+amdz213@users.noreply.github.com>
2023-06-13 18:13:06 +0200

    Update du formulaire

--- Front-end/src/page/validation_form.jsx
+++ Front-end/src/page/validation_form.jsx
@@ -1,59 +1,74 @@
-import React from "react"
+import React, { useState } from "react"
 import { Formik, Form, Field, ErrorMessage } from "formik"
-import { BrowserRouter as Router, Link } from "react-router-dom"
+import { Link } from "react-router-dom"
+import axios from "axios"
+import * as Yup from "yup"
+
+const validationSchema = Yup.object().shape({
+  firstName: Yup.string().required("First name is required"),
+  lastName: Yup.string().required("Last name is required"),
+  address: Yup.string().required("Address is required"),
+  city: Yup.string().required("City is required"),
+  region: Yup.string().required("Region is required"),
+  postalCode: Yup.string()
+    .required("postal code is required")
+    .matches(/^\d{5}$/, "Le code postal doit contenir 5 chiffres"),
+  country: Yup.string().required("Le pays est requis"),
+  phoneNumber: Yup.string()
+    .required("phone number is required")
+    .matches(/^\d{10}$/, "Le numéro de téléphone doit contenir 10 chiffres"),
+})
+
 const ValidateForm = () => {
   const initialValues = {
     firstName: "",
     lastName: "",
-    address1: "",
-    address2: "",
+    address: "",
     city: "",
     region: "",
     postalCode: "",
     country: "",
     phoneNumber: "",
   }
+  const [isFormValid, setIsFormValid] = useState(false)
 
   const validate = (values) => {
-    const errors = {}
-
-    if (!values.firstName) {
-      errors.firstName = "First name is required"
-    }
-
-    if (!values.lastName) {
-      errors.lastName = "Last name is required"
-    }
-
-    if (!values.address1) {
-      errors.address1 = "Address is required"
-    }
-
-    if (!values.city) {
-      errors.city = "City is required"
-    }
-
-    if (!values.region) {
-      errors.region = "Region is required"
-    }
-
-    if (!values.postalCode) {
-      errors.postalCode = "Postal code is required"
-    }
-
-    if (!values.country) {
-      errors.country = "Country is required"
-    }
-
-    if (!values.phoneNumber) {
-      errors.phoneNumber = "Phone number is required"
-    }
-
-    return errors
+    validationSchema
+      .validate(values, { abortEarly: false })
+      .then(() => setIsFormValid(true))
+      .catch(() => setIsFormValid(false))
   }
 
-  const handleSubmit = (values) => {
-    console.log(values)
+  const handleSubmit = async (values, { setSubmitting }) => {
+    try {
+      // Make a POST request to the address API
+      await axios.post("/addresses", {
+        country: values.country,
+        city: values.city,
+        region: values.region,
+        postalCode: values.postalCode,
+      })
+
+      // Make a PATCH request to the user API to update user data
+      await axios.patch("/users/:id", {
+        firstName: values.firstName,
+        lastName: values.lastName,
+        phoneNumber: values.phoneNumber,
+        // Add other user data fields as needed
+      })
+
+      // Handle success or navigate to the next step
+      alert("Address and user data updated successfully")
+      // Navigate to the next step or show a success message
+
+      setSubmitting(false)
+    } catch (error) {
+      // Handle error
+      alert("Failed to update address and user data", error)
+      // Show an error message to the user
+
+      setSubmitting(false)
+    }
   }
 
   return (
@@ -76,8 +91,9 @@ const ValidateForm = () => {
           <h1 className="text-3xl font-bold mb-8">Livraison</h1>
           <Formik
             initialValues={initialValues}
-            validate={validate}
+            validationSchema={validationSchema}
             onSubmit={handleSubmit}
+            validate={validate}
           >
             {({ isSubmitting }) => (
               <Form className="space-y-4">
@@ -123,34 +139,16 @@ const ValidateForm = () => {
 
                 <div>
                   <label htmlFor="address1" className="text-lg font-semibold">
-                    Address 1:
+                    Address :
                   </label>
                   <div className="relative">
                     <Field
                       type="text"
-                      name="address1"
+                      name="address"
                       className="w-full text-black py-1 pl-2 pr-8 rounded-md border border-gray-300 focus:outline-none"
                     />
                     <ErrorMessage
-                      name="address1"
-                      component="div"
-                      className="text-red-500 mt-1"
-                    />
-                  </div>
-                </div>
-
-                <div>
-                  <label htmlFor="address2" className="text-lg font-semibold">
-                    Address 2:
-                  </label>
-                  <div className="relative">
-                    <Field
-                      type="text"
-                      name="address2"
-                      className="w-full text-black py-1 pl-2 pr-8 rounded-md border border-gray-300 focus:outline-none"
-                    />
-                    <ErrorMessage
-                      name="address2"
+                      name="address"
                       component="div"
                       className="text-red-500 mt-1"
                     />
@@ -195,7 +193,10 @@ const ValidateForm = () => {
                 </div>
 
                 <div>
-                  <label htmlFor="postalCode" className="text-lg font-semibold">
+                  <label
+                    htmlFor="postalCode"
+                    className="text-lg font-semibold"
+                  >
                     Postal Code:
                   </label>
                   <div className="relative">
@@ -250,18 +251,21 @@ const ValidateForm = () => {
                     />
                   </div>
                 </div>
-
-                <Link to="/checkout">
-                  {" "}
-                  <button
-                    type="submit"
-                    disabled={isSubmitting}
-                    className="w-full py-2 px-4 btn-black-white font-semibold rounded-md hover:bg-blue-700 focus:outline-none focus:bg-blue-700"
-                    style={{ backgroundColor: "black", color: "white" }}
-                  >
-                    Payment
-                  </button>
-                </Link>
+              
+                <div>
+                    <Link to="/checkout">
+                      <button
+                        type="submit"
+                        disabled={isSubmitting || !isFormValid}
+                        className={`w-full py-2 px-4 btn-black-white font-semibold rounded-md ${
+                          !isFormValid ? "opacity-50 cursor-not-allowed" : ""
+                        } hover:bg-blue-700 focus:outline-none focus:bg-blue-700`}
+                        style={{ backgroundColor: "black", color: "white" }}
+                      >
+                        Payment
+                      </button>
+                    </Link>
+                  </div>
               </Form>
             )}
           </Formik>
